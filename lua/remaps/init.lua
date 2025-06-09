5c8984c8c9cd429b9a77e2bfd13d2aafad8454fa
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2025-06-10 00:20:51 +0530

    Added stuffs for linux

--- lua/remaps/init.lua
+++ lua/remaps/init.lua
@@ -3,6 +3,7 @@ local M = {}
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>wv", vim.cmd.Ex)
 
+vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("v", "<leader><s-y>", "\"+y", { noremap = true, silent = true }) -- Copy to system clipboard
 vim.keymap.set("n", "<leader><s-p>", "\"+p", { noremap = true, silent = true }) -- Paste from system clipboard
 
